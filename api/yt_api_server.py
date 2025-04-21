0961f604fe65db7b7b438c6763c206f723bd059f
MilanPatel2003 <131247110+MilanPatel2003@users.noreply.github.com>
2025-04-21 15:59:34 +0530

    cors solved

--- api/yt_api_server.py
+++ api/yt_api_server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from youtube_transcript_api import YouTubeTranscriptApi
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/yt_api_server')
 def get_transcript():
